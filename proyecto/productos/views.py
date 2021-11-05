102fd65a84c0ed092720a0ecf78a388c9b192ad6
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-05 17:52:56 +0100

    make a view with a template

--- proyecto/productos/views.py
+++ proyecto/productos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def bienvenida(request):
+    return render(
+        request=request,
+        template_name="bienvenida.html",
+        context={
+            "message": "This is a context message",
+            "products": ["TV", "Radio", "Phone"],
+            "error": "Some shit happens"
+        })
--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from productos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bienvenida/', views.bienvenida, name="bienvenida")
 ]
