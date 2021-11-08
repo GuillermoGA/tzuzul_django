3c432e24b45ad2c5070f25dab5cb8937c0aea636
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-08 16:06:53 +0100

    create Producto model and make migrations

--- proyecto/productos/models.py
+++ proyecto/productos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    cantidad = models.IntegerField(default=0)
+    precio = models.FloatField()
+    descripcion = models.TextField(null=True)
+
+    def __str__(self):
+        return f"id:{self.id} ({self.cantidad}) {self.nombre} {self.precio}$"
