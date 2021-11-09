234d3829e5fa35f3f3aadbb70dbc632746140667
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-09 17:39:38 +0100

    productos CRUD

--- proyecto/productos/views.py
+++ proyecto/productos/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
-
+from .models import Producto
 
 # Create your views here.
 def bienvenida(request):
+
+    # SELECT * FROM productos
+    productos = Producto.objects.all()
+
+
+    # SELECT * FROM productos WHERE precio < 5000
+    productos_filtrados = Producto.objects.filter(precio__lte=9000)
+
+    # Crear un formulario, los campos necesarios
+
+    # INSERT INTO productos VALUES()
+    Producto.objects.create(
+        nombre="Consola", 
+        precio=8000, 
+        descripcion="Una consola", 
+        cantidad=10)
+
+    # UPDATE productos WHERE ---
+    productos_filtrados.update(precio=5000)
+
+    # DELETE productos WHERE ---
+    productos.filter(precio__gte=6000).delete()
+
     return render(
         request=request,
         template_name="bienvenida.html",
         context={
             "message": "This is a context message",
-            "products": ["TV", "Radio", "Phone"],
-            "error": "Some shit happens"
+            "productos": productos,
+            "productos_filtrados": productos_filtrados
+            # "error": "Some shit happens"
         })
