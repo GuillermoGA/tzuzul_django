6096f9cdfe42c5daea7c7eedf07f4d4d92d95eb2
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-07 01:20:37 +0100

    read csv file and store data in DB, also prints in index.html

--- avanzado/usuarios/models.py
+++ avanzado/usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    year_birth = models.IntegerField()
+    education = models.CharField(max_length=20)
+    income = models.IntegerField()
--- avanzado/usuarios/views.py
+++ avanzado/usuarios/views.py
@@ -1,31 +1,38 @@
-from django.shortcuts import render
 import csv
-from django.core.files.storage import FileSystemStorage
-# Create your views here.
+from io import StringIO
+
+from django.shortcuts import render
+
+from .models import Usuario
 
 
 def datos_usuarios(request):
     if request.method == "POST":
         file = request.FILES["archivo"]
         try:
-            fs = FileSystemStorage()
-            filename = fs.save(file.name, file)
-            csv_file = open(fs.path(filename))
-            csv_data = csv.reader(csv_file, delimiter='\t')
+            # Get InMemoryUploadedFile data in bytes
+            file_data = file.read()
+
+            # Cast bytes to string by decoding in UTF-8
+            str_file_data = file_data.decode("UTF-8")
+
+            # Create buffer from string
+            file_data_buffer = StringIO(str_file_data)
+
+            # Read csv from buffer
+            csv_data = csv.reader(file_data_buffer)
 
             for line in csv_data:
-                print(line)
+                Usuario.objects.create(
+                    id=line[0],
+                    year_birth=line[1],
+                    education=line[2],
+                    income=line[4]
+                )
         except (IOError, OSError) as file_error:
             print(f"No se puede abrir el archivo. {file_error}")
+        except Exception as e:
+            print(f"Unknown error: {e}")
 
-    try:
-        with open('./usuarios/marketing_campaign.csv') as csv_file:
-            csv_data = csv.reader(csv_file, delimiter='\t')
-
-            data = []
-            for line in csv_data:
-                data.append(line)
-
-    except (IOError, OSError) as file_error:
-        print(f"No se puede abrir el archivo. {file_error}")
+    data = Usuario.objects.all()
     return render(request, 'datos_usuarios.html', {"data": data})
