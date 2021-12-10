163936f2ffe9768680b5baef0d8dc4ef9c4013ca
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-11 00:13:08 +0100

    add django-environment to secure keys

--- avanzado/avanzado/settings.py
+++ avanzado/avanzado/settings.py
@@ -9,11 +9,20 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
